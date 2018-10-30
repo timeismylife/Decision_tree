df5a32673613ee9aee7bbf7a79918635d00db85f
timeismylife <2066009059@qq.com>
2018-10-30 10:20:52 +0800

    add functions
    add some functions to create a tree

--- decision_tree.py
+++ decision_tree.py
@@ -5,19 +5,92 @@ Created on Mon Oct 29 20:58:19 2018
 @author: RUI WANG
 """
 import numpy as np
+import operator
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1,'no']]
+    labels = ['no surfacing','flippers']
+    return dataSet, labels
+
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
     labelCounts = {}
     for featVec in dataSet:
         currentLabel = featVec[-1]
-        if currentLabel not in labelCounts.keys():
-            labelCounts[currentLabel] = 0
-        else:
-            labelCounts[currentLabel] += 1
+        if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
     shannonEnt = 0.0
     for key in labelCounts:
         prob = np.float(labelCounts[key])/numEntries
         shannonEnt -= prob*np.log2(prob) 
     return shannonEnt
-            
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+           reducedFeatVec = featVec[:axis]
+           reducedFeatVec.extend(featVec[axis+1:])
+           retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+def chooseBestFeaturetoSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGan = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVal = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVal:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet)/np.float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGan = baseEntropy - newEntropy
+        if (infoGan > bestInfoGan):
+           bestInfoGan = infoGan
+           bestFeature = i
+    return bestFeature
+    
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1),
+                              reverse = True)
+    return sortedClassCount[0][0]
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeaturetoSplit(dataSet)
+    bestLabel = labels[bestFeat]
+    myTree = {bestLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueValues = set(featValues)
+    for value in uniqueValues:
+        subLabels = labels[:]
+        myTree[bestLabel][value] = createTree(splitDataSet(dataSet,bestFeat,value),
+              subLabels)
+    return myTree
+    
+         
+    
+
+dataSet, labels = createDataSet()
+shannonEnt = calcShannonEnt(dataSet)
+splitDataSet(dataSet, 0, 1)
+feat = chooseBestFeaturetoSplit(dataSet)
+myTree = createTree(dataSet, labels)
+
+
         
